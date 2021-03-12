e9caefcc5b8de59e8ffad14d3e9f77c6d46b2603
Anton Lavrenov <lavrton@gmail.com>
2021-03-12 15:28:47 -0500

    add demos

---
+++ examples/polotno-demo/src/index.js
@@ -0,0 +1,11 @@
+import { createDemoApp } from 'polotno/polotno-app';
+
+console.log('starting the app...');
+
+const { store } = createDemoApp({
+  container: document.getElementById('root'),
+  // this is a demo key just for that project
+  // (!) please don't use it in your projects
+  // to create your own API key please go here: https://polotno.dev/cabinet
+  key: 'nFA5H9elEytDyPyvKL7T',
+});
---
+++ examples/polotno-simple-t-shirt-demo/src/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Workspace from "polotno/canvas/workspace";
+import { createStore } from "polotno/model/store";
+
+const store = createStore({
+  // this is a demo key just for that project
+  // (!) please don't use it in your projects
+  // to create your own API key please go here: https://polotno.dev/cabinet
+  key: "nFA5H9elEytDyPyvKL7T"
+});
+store.setSize(500, 500);
+const page = store.addPage();
+
+page.set({
+  background: "/tshirt.png"
+});
+
+const text = page.addElement({
+  x: 150,
+  y: 170,
+  fontSize: 25,
+  type: "text",
+  fill: "black",
+  text: "The best t-shirt owner",
+  width: 200
+});
+
+const App = () => {
+  return (
+    <div>
+      <div style={{ width: "100vw", height: "50px", textAlign: "center" }}>
+        <input
+          placeholder="Type t-shirt text"
+          style={{
+            padding: "5px",
+            width: "60%",
+            margin: "10px"
+          }}
+          onChange={(e) => {
+            text.set({ text: e.target.value });
+          }}
+        />
+        <button
+          onClick={() => {
+            store.saveAsImage({ ignoreBackground: true });
+          }}
+        >
+          Download print
+        </button>
+      </div>
+      <div style={{ width: "100vw", height: "calc(100vh - 50px)" }}>
+        <Workspace store={store} />
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
