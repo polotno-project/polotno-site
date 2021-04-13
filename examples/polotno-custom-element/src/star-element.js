301c746aa89d39dc94f7af3005d011ac67b36bb5
Anton Lavrenov <lavrton@gmail.com>
2021-04-13 18:08:14 -0500

    update demo

--- examples/polotno-custom-element/src/star-element.js
+++ examples/polotno-custom-element/src/star-element.js
@@ -1,36 +1,32 @@
-import React from "react";
+import React from 'react';
 // polotno is made with mobx library
 // we will need its tools to make reactive components
-import { observer } from "mobx-react-lite";
+import { observer } from 'mobx-react-lite';
 // import Konva components
-import { Star } from "react-konva";
+import { Star } from 'react-konva';
 
 // import toolbar components
-import { NumericInput, Navbar, Alignment } from "@blueprintjs/core";
-import ColorPicker from "polotno/toolbar/color-picker";
+import { NumericInput, Navbar, Alignment } from '@blueprintjs/core';
+import ColorPicker from 'polotno/toolbar/color-picker';
 
 // import Polotno API methods
-import { unstable_registerShapeComponent } from "polotno/config";
-import { unstable_registerToolbarComponent } from "polotno/config";
-import { unstable_registerShapeModel } from "polotno/config";
-import { unstable_registerTransformerAttrs } from "polotno/config";
+import { unstable_registerShapeComponent } from 'polotno/config';
+import { unstable_registerToolbarComponent } from 'polotno/config';
+import { unstable_registerShapeModel } from 'polotno/config';
+import { unstable_registerTransformerAttrs } from 'polotno/config';
 
-// use default snap
-import { useSnap } from "polotno/canvas/use-snap";
-
+// define our model
+// we need to provide all default values
 unstable_registerShapeModel({
-  type: "star",
+  type: 'star',
   radius: 100,
-  fill: "black",
-  numPoints: 6
+  fill: 'black',
+  numPoints: 6,
 });
 
-// every components has three main props - onClick, element and store
-export const StarElement = observer(({ onClick, element, store }) => {
+// now we need to define how elements looks on canvas
+export const StarElement = observer(({ element, store }) => {
   const ref = React.useRef(null);
-
-  // useSnap - is utility hook that automatically enables snapping
-  const { onDragMove, onDragEnd } = useSnap(ref);
 
   const handleChange = (e) => {
     const node = e.currentTarget;
@@ -39,11 +35,12 @@ export const StarElement = observer(({ onClick, element, store }) => {
     // we don't need that, so we reset it back to 1.
     node.scaleX(1);
     node.scaleY(1);
+    // and then save all changes back to the model
     element.set({
       x: node.x(),
       y: node.y(),
       rotation: e.target.rotation(),
-      radius: element.radius * scaleX
+      radius: element.radius * scaleX,
     });
   };
 
@@ -53,7 +50,10 @@ export const StarElement = observer(({ onClick, element, store }) => {
   return (
     <Star
       ref={ref}
+      // remember to use "element" name. Polotno will use it internally to find correct node
       name="element"
+      // also it is important to pass id
+      // so polotno can automatically do selection
       id={element.id}
       x={element.x}
       y={element.y}
@@ -65,35 +65,20 @@ export const StarElement = observer(({ onClick, element, store }) => {
       draggable={!element.locked}
       outerRadius={element.radius}
       innerRadius={element.radius * 0.5}
-      onDragStart={() => {
-        const isSelected = store.selectedElements.find((e) => e === element);
-        if (!isSelected) {
-          store.selectElements([element.id]);
-        }
-      }}
-      onDragMove={(e) => {
-        onDragMove(e);
-        handleChange(e);
-      }}
-      onDragEnd={(e) => {
-        onDragEnd(e);
-      }}
-      onClick={() => {
-        onClick();
-      }}
-      onTap={() => {
-        onClick();
-      }}
+      onDragMove={handleChange}
       onTransform={handleChange}
     />
   );
 });
 
-unstable_registerShapeComponent("star", StarElement);
-unstable_registerTransformerAttrs("star", {
-  enabledAnchors: ["top-left", "top-right", "bottom-left", "bottom-right"]
+// now we can register canvas component
+unstable_registerShapeComponent('star', StarElement);
+// and change default transformer a bit
+unstable_registerTransformerAttrs('star', {
+  enabledAnchors: ['top-left', 'top-right', 'bottom-left', 'bottom-right'],
 });
 
+// now we can define custom toolbar
 const LineToolbar = observer(({ store }) => {
   const element = store.selectedElements[0];
 
@@ -103,7 +88,7 @@ const LineToolbar = observer(({ store }) => {
         value={element.fill}
         onChange={(fill) =>
           element.set({
-            fill
+            fill,
           })
         }
       />
@@ -112,7 +97,7 @@ const LineToolbar = observer(({ store }) => {
           element.set({ radius: val });
         }}
         value={element.radius}
-        style={{ width: "50px", marginLeft: "10px" }}
+        style={{ width: '50px', marginLeft: '10px' }}
         min={1}
         max={500}
       />
@@ -120,4 +105,4 @@ const LineToolbar = observer(({ store }) => {
   );
 });
 
-unstable_registerToolbarComponent("star", LineToolbar);
+unstable_registerToolbarComponent('star', LineToolbar);
