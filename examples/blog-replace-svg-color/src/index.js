cdef91e18d8ce65bd96a131f7054f52531e13e3e
Anton Lavrenov <lavrton@gmail.com>
2021-04-26 10:34:52 -0500

    fix demo

--- examples/blog-replace-svg-color/src/index.js
+++ examples/blog-replace-svg-color/src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { render } from "react-dom";
-import { Stage, Layer, Image } from "react-konva";
-import useImage from "use-image";
-import { HexColorPicker } from "react-colorful";
-import * as svg from "./svg";
+import React from 'react';
+import { render } from 'react-dom';
+import { Stage, Layer, Image } from 'react-konva';
+import useImage from 'use-image';
+import { HexColorPicker } from 'react-colorful';
+import * as svg from './svg';
 
 const ORIGINAL_SVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><g data-name="discount(Splash tag)"><path fill="#db5669" d="M45.93,26.39a3.2,3.2,0,0,0-.76,3.79,3.21,3.21,0,0,1-1.83,4.42,3.19,3.19,0,0,0-2.15,3c0,.06,0,.36,0,.39a3.21,3.21,0,0,1-3.4,3.2,3.19,3.19,0,0,0-3.21,2.14,3.2,3.2,0,0,1-4.42,1.83,3.2,3.2,0,0,0-3.79.76,3.2,3.2,0,0,1-4.78,0,3.2,3.2,0,0,0-3.79-.76,3.2,3.2,0,0,1-4.42-1.83,3.18,3.18,0,0,0-3.21-2.14A3.2,3.2,0,0,1,6.8,37.81,3.19,3.19,0,0,0,4.66,34.6a3.2,3.2,0,0,1-1.83-4.42,3.2,3.2,0,0,0-.76-3.79,3.2,3.2,0,0,1,0-4.78,3.2,3.2,0,0,0,.76-3.79A3.2,3.2,0,0,1,4.66,13.4,3.18,3.18,0,0,0,6.8,10.19,3.2,3.2,0,0,1,10.1,6.8h.29a3.19,3.19,0,0,0,3-2.15,3.21,3.21,0,0,1,4.42-1.83,3.2,3.2,0,0,0,3.79-.76,3.2,3.2,0,0,1,4.78,0,3.21,3.21,0,0,0,3.79.76A3.2,3.2,0,0,1,34.6,4.66a3.19,3.19,0,0,0,3,2.15l.39,0a3.21,3.21,0,0,1,3.2,3.4,3.19,3.19,0,0,0,2.14,3.21,3.19,3.19,0,0,1,2.15,3c0,1.29-.64,1.52-.64,2.79C44.85,21.68,47,21.53,47,24A3.19,3.19,0,0,1,45.93,26.39Z"/><path fill="#f26674" d="M45.93,26.39a3.2,3.2,0,0,0-.76,3.79,3.12,3.12,0,0,1,.18,2.3c0,.1-.76,1.28-.82,1.38a3.08,3.08,0,0,1-1.19.74,3.19,3.19,0,0,0-2.15,3s0,.14,0,.29C27.32,51.18,4,41.37,4,22A21.9,21.9,0,0,1,10.1,6.8a3.2,3.2,0,0,0,3.3-2.14c.38-1.13,1-1.39,2.11-2a2.89,2.89,0,0,1,.91-.14c1.29,0,1.52.64,2.79.64C21.68,3.15,21.53,1,24,1a3.19,3.19,0,0,1,2.39,1.07,3.21,3.21,0,0,0,3.79.76A3.2,3.2,0,0,1,34.6,4.66,3.18,3.18,0,0,0,37.81,6.8a3.2,3.2,0,0,1,3.39,3.39,3.19,3.19,0,0,0,2.14,3.21,3.2,3.2,0,0,1,1.83,4.42,3.2,3.2,0,0,0,.76,3.79A3.2,3.2,0,0,1,45.93,26.39Z"/><path fill="#c4455e" d="M41,24A17,17,0,0,1,24,41C8.17,41,1,21.21,13,11,23.87,1.82,41,9.36,41,24Z"/><path fill="#db5669" d="M41,24a16.91,16.91,0,0,1-4,11,16.91,16.91,0,0,1-11,4C11.38,39,3.81,21.88,13,11,23.87,1.82,41,9.36,41,24Z"/><path fill="#ffde76" d="M16 33a1 1 0 0 1-.71-1.71l16-16a1 1 0 0 1 1.42 1.42C15.31 34.1 16.59 33 16 33zM19 22a4 4 0 1 0-4-4A4 4 0 0 0 19 22zm0-6a2 2 0 1 1-2 2A2 2 0 0 1 19 16zM29 26a4 4 0 1 0 4 4A4 4 0 0 0 29 26zm0 6a2 2 0 1 1 2-2A2 2 0 0 1 29 32z"/></g></svg>`;
 
@@ -15,8 +15,8 @@ const App = () => {
   const [displayColorPicker, setDisplayColorPicker] = React.useState(false);
   const [
     colorPickerSelectedColor,
-    setColorPickerSelectedColor
-  ] = React.useState("#ffffff");
+    setColorPickerSelectedColor,
+  ] = React.useState('#ffffff');
 
   const showColorPicker = (toggle, oldColor) => {
     setDisplayColorPicker(toggle);
@@ -26,23 +26,23 @@ const App = () => {
   const setNewColor = (oldColor, newColor) => {
     setColorMap({
       ...colorMap,
-      [oldColor]: newColor
+      [oldColor]: newColor,
     });
   };
 
   return (
     <div
       style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "rebeccapurple"
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'rebeccapurple',
       }}
     >
-      <h3 style={{ fontFamily: "sans-serif", color: "thistle" }}>
+      <h3 style={{ fontFamily: 'sans-serif', color: 'thistle' }}>
         Try to change these colors
       </h3>
       <div>
@@ -51,12 +51,12 @@ const App = () => {
             <>
               <div
                 style={{
-                  content: "",
-                  width: "30px",
-                  height: "30px",
+                  content: '',
+                  width: '30px',
+                  height: '30px',
                   backgroundColor: colorMap[color] || color,
-                  display: "inline-block",
-                  marginRight: "10px"
+                  display: 'inline-block',
+                  marginRight: '10px',
                 }}
                 onClick={() => showColorPicker(true, color)}
               ></div>
@@ -72,7 +72,7 @@ const App = () => {
           />
         )}
       </>
-      <Stage width={image && image.width} height={image && image.height}>
+      <Stage width={300} height={300}>
         <Layer>
           <Image image={image} />
         </Layer>
@@ -81,4 +81,4 @@ const App = () => {
   );
 };
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
