8004d1c4a4c599a49f67b3b5be8d03a02a94e027
Anton Lavrenov <lavrton@gmail.com>
2021-07-08 01:30:55 +0700

    update last demo

--- examples/polotno-custom-side-panel/src/index.js
+++ examples/polotno-custom-side-panel/src/index.js
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { observer } from "mobx-react-lite";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { observer } from 'mobx-react-lite';
 
-import { Workspace } from "polotno/canvas/workspace";
-import { SidePanel } from "polotno/side-panel";
-import { Toolbar } from "polotno/toolbar/toolbar";
-import { ZoomButtons } from "polotno/toolbar/zoom-buttons";
-import { createStore } from "polotno/model/store";
+import { PolotnoContainer, SidePanelWrap, WorkspaceWrap } from 'polotno';
+
+import { Workspace } from 'polotno/canvas/workspace';
+import { SidePanel } from 'polotno/side-panel';
+import { Toolbar } from 'polotno/toolbar/toolbar';
+import { ZoomButtons } from 'polotno/toolbar/zoom-buttons';
+import { createStore } from 'polotno/model/store';
 
 // import all default sections
 // we will not use all of them in the demo
@@ -17,29 +19,29 @@ import {
   ElementsSection,
   UploadSection,
   BackgroundSection,
-  SizeSection
-} from "polotno/side-panel";
+  SizeSection,
+} from 'polotno/side-panel';
 //
-import { SectionTab } from "polotno/side-panel";
+import { SectionTab } from 'polotno/side-panel';
 // import our own icon
-import FaShapes from "@meronex/icons/fa/FaShapes";
+import FaShapes from '@meronex/icons/fa/FaShapes';
 
 const store = createStore({
   // this is a demo key just for that project
   // (!) please don't use it in your projects
   // to create your own API key please go here: https://polotno.dev/cabinet
-  key: "nFA5H9elEytDyPyvKL7T"
+  key: 'nFA5H9elEytDyPyvKL7T',
 });
 
 store.addPage();
 store.activePage.addElement({
-  type: "text",
-  text: "hello"
+  type: 'text',
+  text: 'hello',
 });
 
 // define the new custom section
 const CustomSection = {
-  name: "custom",
+  name: 'custom',
   Tab: (props) => (
     <SectionTab name="Custom" {...props}>
       <FaShapes icon="new-text-box" />
@@ -53,7 +55,7 @@ const CustomSection = {
         <p>Elements on the current page: {store.activePage?.children.length}</p>
       </div>
     );
-  })
+  }),
 };
 
 // we will have just two sections
@@ -61,32 +63,17 @@ const sections = [CustomSection, TextSection];
 
 export const App = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        height: "100%",
-        width: "100%"
-      }}
-    >
-      <div style={{ width: "400px", height: "100%", display: "flex" }}>
+    <PolotnoContainer className="polotno-app-container">
+      <SidePanelWrap>
         <SidePanel store={store} sections={sections} defaultSection="custom" />
-      </div>
-      <div
-        style={{
-          display: "flex",
-          height: "100%",
-          margin: "auto",
-          flex: 1,
-          flexDirection: "column",
-          position: "relative"
-        }}
-      >
-        <Toolbar store={store} downloadButtonEnabled={true} />
+      </SidePanelWrap>
+      <WorkspaceWrap>
+        <Toolbar store={store} downloadButtonEnabled />
         <Workspace store={store} />
         <ZoomButtons store={store} />
-      </div>
-    </div>
+      </WorkspaceWrap>
+    </PolotnoContainer>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
