ab27ac261f1e0c291d9b49c303c44256dc793838
Anton Lavrenov <lavrton@gmail.com>
2021-07-13 00:01:32 +0700

    update demos

--- examples/polotno-and-vue/src/App.vue
+++ examples/polotno-and-vue/src/App.vue
@@ -8,6 +8,10 @@
       href="https://unpkg.com/@blueprintjs/core@^3.10.0/lib/css/blueprint.css"
       rel="stylesheet"
     />
+    <link
+      href="https://unpkg.com/@blueprintjs/popover2@0.11.0/lib/css/blueprint-popover2.css"
+      rel="stylesheet"
+    />
     <!-- <div v-text="store.pages[0].children.length"></div> -->
     <div id="polotno"></div>
   </div>
