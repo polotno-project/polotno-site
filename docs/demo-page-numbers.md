a8c751150bdbf45b39bae81ad89df11c7b95470a
Anton Lavrenov <lavrton@gmail.com>
2022-05-05 23:49:14 -0500

    --amend

---
+++ docs/demo-page-numbers.md
@@ -0,0 +1,26 @@
+---
+title: Page numbers
+hide_table_of_contents: true
+---
+
+What if you want to automatically add page numbers to your document?
+
+Usually most of elements in the store are controlled by the user. But using `Polotno` you can create your own elements and fully control them programmatically.
+In this demo we will add a special hook to show pages numbers directly on the canvas.
+
+_Instructions: See number on the first page. Try to add/reorder/remove pages. You will see page numbers are updated automatically._
+
+<p><a className="button button--primary" href="https://codesandbox.io/s/github/polotno-project/polotno-site/tree/source/examples/polotno-page-numbers" target="_blank">Open Demo</a></p>
+
+<iframe
+    src="https://codesandbox.io/embed/github/polotno-project/polotno-site/tree/source/examples/polotno-page-numbers?fontsize=11&hidenavigation=1&theme=dark&view=preview"
+    style={{
+      width: '100%',
+      height: '700px',
+      border: 0,
+      overflow: 'hidden',
+    }}
+    title="Polotno demo"
+    allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb"
+    sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin allow-downloads"
+  ></iframe>
---
+++ examples/polotno-page-numbers/src/index.js
@@ -0,0 +1,75 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { PolotnoContainer, SidePanelWrap, WorkspaceWrap } from "polotno";
+
+import { Workspace } from "polotno/canvas/workspace";
+import { SidePanel } from "polotno/side-panel";
+import { Toolbar } from "polotno/toolbar/toolbar";
+import { ZoomButtons } from "polotno/toolbar/zoom-buttons";
+import { createStore } from "polotno/model/store";
+
+// create store
+const store = createStore({
+  // this is a demo key just for that project
+  // (!) please don't use it in your projects
+  // to create your own API key please go here: https://polotno.dev/cabinet
+  key: "nFA5H9elEytDyPyvKL7T",
+  // you can hide back-link on a paid licence
+  // but it will be good if you can keep it for Polotno project support
+  showCredit: true
+});
+// add to global namespace for debugging
+window.store = store;
+
+// add page and element instantly
+store.addPage();
+
+const checkPageNumbers = () => {
+  // check every page
+  store.pages.forEach((page, index) => {
+    // lets try to find existing page number element
+    let pageNumber = page.children.find(
+      (el) => el.custom?.name === "page-number"
+    );
+    const text = `Page ${index + 1}`;
+    if (pageNumber) {
+      // if such element is already here, let's update it
+      // in the case if a user reoderer pages
+      pageNumber.set({ text });
+    } else {
+      // or just create a new one
+      page.addElement({
+        type: "text",
+        custom: { name: "page-number" },
+        text,
+        width: store.width,
+        align: "center",
+        fontSize: 40,
+        x: 0,
+        y: store.height - 50,
+        selectable: false,
+        alwaysOnTop: true
+      });
+    }
+  });
+};
+
+checkPageNumbers();
+store.on("change", checkPageNumbers);
+
+export const App = () => {
+  return (
+    <PolotnoContainer className="polotno-app-container">
+      <SidePanelWrap>
+        <SidePanel store={store} />
+      </SidePanelWrap>
+      <WorkspaceWrap>
+        <Toolbar store={store} />
+        <Workspace store={store} />
+        <ZoomButtons store={store} />
+      </WorkspaceWrap>
+    </PolotnoContainer>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
