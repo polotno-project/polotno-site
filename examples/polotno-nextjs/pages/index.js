e410d4dbfe0fbc31190e19bf3ff0db4c861fad3d
Anton Lavrenov <lavrton@gmail.com>
2022-10-12 13:13:54 -0500

    fix style a bit

--- examples/polotno-nextjs/pages/index.js
+++ examples/polotno-nextjs/pages/index.js
@@ -1,12 +1,18 @@
-import dynamic from "next/dynamic";
-const Editor = dynamic(() => import("../components/editor"), {
-  ssr: false
+import dynamic from 'next/dynamic';
+const Editor = dynamic(() => import('../components/editor'), {
+  ssr: false,
 });
 
 export default function IndexPage() {
   return (
     <div>
       <Editor />
+      <style jsx global>{`
+        body {
+          padding: 0;
+          margin: 0;
+        }
+      `}</style>
     </div>
   );
 }
